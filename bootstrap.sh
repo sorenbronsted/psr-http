8901184f9802db5e1e71de5a521319f4e65da56e
Søren Brønsted <soren@bronsted.dk>
2022-03-01 20:02:48 +0100

    WIP

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 apt-get update
-apt-get install -y php-cli php-xdebug php-xml php-mbstring composer
+apt-get install -y php-cli php-xdebug php-xml php-mbstring php-curl composer
